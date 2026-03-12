e4f75440c700fb9321586e80431779a324e928ae
isguliyev <isguliyev24@gmail.com>
2026-03-12 14:33:04 +0400

    refactor(cookie): rename boxQuantity to boxCount and add equals/hashCode to CookieOrder

--- 0x09-collections-list/src/main/java/io/github/isguliyev/examples/cookie/CookieOrder.java
+++ 0x09-collections-list/src/main/java/io/github/isguliyev/examples/cookie/CookieOrder.java
@@ -1,19 +1,39 @@
 package io.github.isguliyev.examples.cookie;
 
+import java.util.Objects;
+
 public class CookieOrder {
     private final String flavor;
-    private final int boxQuantity;
+    private final int boxCount;
 
-    public CookieOrder(String flavor, int boxQuantity) {
+    public CookieOrder(String flavor, int boxCount) {
         this.flavor = flavor;
-        this.boxQuantity = boxQuantity;
+        this.boxCount = boxCount;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (!(object instanceof CookieOrder cookieOrder)) {
+            return false;
+        }
+
+        return this.boxCount == cookieOrder.boxCount && Objects.equals(this.flavor, cookieOrder.flavor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.flavor, this.boxCount);
     }
 
     public String getFlavor() {
         return this.flavor;
     }
 
-    public int getBoxQuantity() {
-        return this.boxQuantity;
+    public int getBoxCount() {
+        return this.boxCount;
     }
 }
