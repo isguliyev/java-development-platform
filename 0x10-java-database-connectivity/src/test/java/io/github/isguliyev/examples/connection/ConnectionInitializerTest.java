7f87e6d209ed9d9dfc1e0fa66cfe3a69f6ea2203
isguliyev <isguliyev24@gmail.com>
2026-03-17 01:13:14 +0400

    refactor(connection): remove setUp method and make DATABASE_NAME constant

--- 0x10-java-database-connectivity/src/test/java/io/github/isguliyev/examples/connection/ConnectionInitializerTest.java
+++ 0x10-java-database-connectivity/src/test/java/io/github/isguliyev/examples/connection/ConnectionInitializerTest.java
@@ -1,7 +1,6 @@
 package io.github.isguliyev.examples.connection;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,34 +11,27 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ConnectionInitializerTest {
-    private final String databaseName = "test.db";
-    private File file;
-
-    @BeforeEach
-    void setUp() {
-        this.file = new File(this.databaseName);
-
-        if (this.file.exists()) {
-            this.file.delete();
-        }
-    }
+    private static final String DATABASE_NAME = "test.db";
+    private final File databaseFile = new File(DATABASE_NAME);
 
     @AfterEach
     void tearDown() {
-        this.file.delete();
+        if (this.databaseFile.exists()) {
+            this.databaseFile.delete();
+        }
     }
 
     @Test
     void getSQLiteConnection_returnsConnectionObject() throws SQLException {
-        try (Connection connection = ConnectionInitializer.getSQLiteConnection(this.databaseName)) {
+        try (Connection connection = ConnectionInitializer.getSQLiteConnection(DATABASE_NAME)) {
             assertTrue(connection.isValid(1));
         }
     }
 
     @Test
     void getSQLiteConnection_createsDatabaseFile() throws SQLException {
-        try (Connection connection = ConnectionInitializer.getSQLiteConnection(this.databaseName)) {
-            assertTrue(this.file.exists());
+        try (Connection connection = ConnectionInitializer.getSQLiteConnection(DATABASE_NAME)) {
+            assertTrue(this.databaseFile.exists());
         }
     }
 }
