e3a36f691c673dadfa0054d6cc3bfa9bdb065a9e
isguliyev <isguliyev24@gmail.com>
2026-03-18 00:07:34 +0400

    test(serialization): refactor test setup and improve exception specificity

--- 0x0F-serialization/src/test/java/io/github/isguliyev/examples/serialization/SerializerTest.java
+++ 0x0F-serialization/src/test/java/io/github/isguliyev/examples/serialization/SerializerTest.java
@@ -2,6 +2,7 @@ package io.github.isguliyev.examples.serialization;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,29 +12,35 @@ import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+import java.nio.file.Path;
+import java.nio.file.Files;
 
 public class SerializerTest {
-    private String fileName;
-    private Serializer<Employee> serializer;
+    private static final String FILE_NAME = "employees.data";
+    private Serializer<Employee> serializer = new Serializer<Employee>(FILE_NAME);
     private List<Employee> employees;
 
     @BeforeEach
     void setUp() {
-        this.fileName = "employees.data";
-        this.serializer = new Serializer<Employee>(this.fileName);
-
         this.employees = List.of(
             new Employee("Adrian Shephard", "pipeWrench"),
             new Employee("Gordon Freeman", "crowbar")
         );
     }
 
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(Path.of(FILE_NAME));
+    }
+
     @Test
-    void serialize_serializesListOfObjects() throws Exception {
+    void serialize_serializesListOfObjects() throws IOException, ClassNotFoundException {
         this.serializer.serialize(this.employees);
 
         try (
-            final FileInputStream fileInputStream = new FileInputStream(this.fileName);
+            final FileInputStream fileInputStream = new FileInputStream(FILE_NAME);
             final ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)
         ) {
             assertEquals(this.employees, (List<Employee>) objectInputStream.readObject());
@@ -41,9 +48,9 @@ public class SerializerTest {
     }
 
     @Test
-    void deserialize_deserializesListOfObjectsFromFile() throws Exception {
+    void deserialize_deserializesListOfObjectsFromFile() throws IOException, ClassNotFoundException {
         try (
-            final FileOutputStream fileOutputStream = new FileOutputStream(this.fileName);
+            final FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME);
             final ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)
         ) {
             objectOutputStream.writeObject(this.employees);
